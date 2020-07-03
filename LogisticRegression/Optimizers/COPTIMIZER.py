47a43cae9e1876a8f20fb089b61af2e9566bba6c
Muhammad Ibrahim Qureshi <67155718+qureshi-mi@users.noreply.github.com>
2020-07-02 22:18:05 -0400

    Add files via upload

---
+++ LogisticRegression/Optimizers/COPTIMIZER.py
@@ -0,0 +1,45 @@
+################################################################################################################################
+##---------------------------------------------------Centralized Optimizers---------------------------------------------------##
+################################################################################################################################
+
+import numpy as np
+import copy as cp
+import utilities as ut
+
+## Centralized gradient descent
+def CGD(pr,learning_rate,K,theta_0):
+    theta = [theta_0]  
+    for k in range(K):
+        theta.append( theta[-1] - learning_rate * pr.grad(theta[-1]) )
+        ut.monitor('CGD',k,K)
+    theta_opt = theta[-1]
+    F_opt = pr.F_val(theta[-1])
+    return theta, theta_opt, F_opt
+
+## Centralized gradient descent with momentum
+def CNGD(pr,learning_rate,momentum,K,theta_0):
+    theta = [theta_0]  
+    theta_aux = cp.deepcopy(theta_0)
+    for k in range(K):
+        grad = pr.grad(theta[-1])
+        theta_aux_last = cp.deepcopy(theta_aux)
+        theta_aux = theta[-1] - learning_rate * grad 
+        theta.append( theta_aux + momentum * ( theta_aux - theta_aux_last ) )
+        ut.monitor('CNGD',k,K)
+    theta_opt = theta[-1]
+    F_opt = pr.F_val(theta[-1])
+    return theta, theta_opt, F_opt
+
+## Centralized stochastic gradient descent
+def CSGD(pr,learning_rate,K,theta_0):
+    N = pr.N
+    theta = cp.deepcopy(theta_0)
+    theta_epoch = [ theta_0 ]
+    for k in range(K):
+        idx = np.random.randint(0,N)
+        grad = pr.grad(theta,idx)
+        theta -= learning_rate * grad 
+        if (k+1) % N == 0:
+            theta_epoch.append( cp.deepcopy(theta) )
+        ut.monitor('CSGD',k,K)
+    return theta_epoch
