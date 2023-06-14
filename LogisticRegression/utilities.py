25e44666498875fc191bc1e90158f0f17559fa33
U1ltra <118020229@link.cuhk.edu.cn>
2023-06-14 13:05:47 -0400

    misc: load and train util

--- LogisticRegression/utilities.py
+++ LogisticRegression/utilities.py
@@ -114,6 +114,9 @@ def load_state(save_path, exp_name):
     theta = np.load(f"{save_path}/{exp_name}_theta.npy")
     return theta, theta[-1]  # return the state sequence and the last state (optimum)
 
+def load_optimal(save_path, exp_name):
+    return np.load(save_path, exp_name)
+    
 def loadPathAndPlot(save_path, exp_names, error_lr, plot_every):
     load_thetas = []
     print("loading theta training results...")
