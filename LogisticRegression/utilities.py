ec2d56e9e9d31d493d14f25c379df0367133fa50
U1ltra <118020229@link.cuhk.edu.cn>
2023-07-10 22:02:49 -0400

    feat: generate fixed lambda matrices

--- LogisticRegression/utilities.py
+++ LogisticRegression/utilities.py
@@ -221,8 +221,8 @@ def spectral_norm(comm_matrix):
 
     return spectral_norm
 
-def print_matrix(matrix):
-    print("Matrix:")
+def print_matrix(matrix, name):
+    print(f"{name} Matrix:")
     for row in matrix:
         for element in row:
             if element < 0:
@@ -231,6 +231,20 @@ def print_matrix(matrix):
                 print(f" {element:.3f}", end=" ")
         print()
     print()
+
+def is_primitive(matrix, max_iterations=10000):
+    n = matrix.shape[0]  # Assuming matrix is a square matrix
+    power_counter = 1
+
+    while power_counter <= max_iterations:
+        power_matrix = np.linalg.matrix_power(matrix, power_counter)
+        if np.any(power_matrix <= 0):
+            return False
+        elif np.all(power_matrix > 0):
+            return True
+        power_counter += 1
+
+    return False
 
 def convergence_analysis():
     lr_constant = False
@@ -320,20 +334,116 @@ def convergence_analysis():
                     print(f"{algo:<5}: Error floor: {error_floor} ({net})")
 
 
-def try_geo():
-    node_num = 16
-    matrices = []
-    norms = []
-    for i in range(1000):
-        undir_graph = Geometric_graph(
-            node_num
-        ).undirected(0.8)
+def try_geo(save_path):
+    start = time.time()
 
+    np.random.seed(0)
+    nodes_list = [8, 16, 32]
+    matrices_lambda = []
+    search_space = int(1e5)
+
+    print("generating matrices...", flush=True)
+    for node_num in nodes_list:
+        print(f"node_num = {node_num}", flush=True) 
+        mat = []
+        for i in range(search_space):
+            undir_graph = Geometric_graph(
+                node_num
+            ).undirected(0.8)
+
+            communication_matrix = Weight_matrix(undir_graph).row_stochastic()
+            communication_matrix = convert_to_doubly_stochastic(communication_matrix, int(1e4), 1e-7)
+
+            norm = spectral_norm(communication_matrix)
+            mat.append((node_num, communication_matrix, norm))
+
+            if i % (search_space / 10) == 0:
+                print(f"{i / search_space * 100}% completed", flush=True)
+
+        matrices_lambda.append(mat)
+    print("time elapsed: ", time.time() - start, flush=True)
+
+    print("searching for min distance... (1)", flush=True)
+    min_dist = 1e10
+    min_dist_tup = None
+    min_dist_list = []
+    for idx1, mat1 in enumerate(matrices_lambda[0]):
+        for idx2, mat2 in enumerate(matrices_lambda[1]):
+            dist = abs(mat1[2] - mat2[2])
+            if dist < min_dist:
+                min_dist = dist
+                min_dist_tup = (mat1, mat2)
+                min_dist_list.append((mat1, mat2))
+        if idx1 % (search_space / 10) == 0:
+            print(f"{idx1 / search_space * 100}% completed", flush=True)
+
+    print(f"min_dist = {min_dist} (1)", flush=True)
+    print(f"lambdas = {min_dist_tup[0][2]}, {min_dist_tup[1][2]}", flush=True)
+    print("time elapsed: ", time.time() - start, flush=True)
+    
+    print("searching for min distance... (2)", flush=True)
+    min_dist = 1e10
+    min_dist_tup = None
+    min_dist_list_2 = []
+    for idx3, mat3 in enumerate(matrices_lambda[2]):
+        dist1 = abs(mat3[2] - min_dist_list[-1][0][2])
+        dist2 = abs(mat3[2] - min_dist_list[-1][1][2])
+        dist = min(dist1, dist2)        
+        if dist < min_dist:
+            min_dist = dist
+            min_dist_tup = (*min_dist_list[-1], mat3)
+            min_dist_list_2.append(min_dist_tup)
+        if idx3 % (search_space / 10) == 0:
+            print(f"{idx3 / search_space * 100}% completed", flush=True)
+    
+    print(f"min_dist = {min_dist} (2)", flush=True)
+    print(f"lambdas = {min_dist_tup[0][2]}, {min_dist_tup[1][2]}, {min_dist_tup[2][2]}", flush=True)
+    print("time elapsed: ", time.time() - start, flush=True)
+
+    # save the closest 100 matrices
+    print("saving matrices...", flush=True)
+    if not os.path.exists(f"{save_path}/geo"):
+        os.mkdir(f"{save_path}/geo")
+
+    for idx, tup in enumerate(min_dist_list_2[-min(100, len(min_dist_list_2)):]):
+        np.save(f"{save_path}/geo/geo_{idx}_node{nodes_list[0]}", tup[0][1])
+        np.save(f"{save_path}/geo/geo_{idx}_node{nodes_list[1]}", tup[1][1])
+        np.save(f"{save_path}/geo/geo_{idx}_node{nodes_list[2]}", tup[2][1])
+
+    # save the closest 100 lambdas
+    print("saving lambdas...", flush=True)
+    for idx, tup in enumerate(min_dist_list_2[-min(100, len(min_dist_list_2)):]):
+        np.save(f"{save_path}/geo/geo_{idx}_node{nodes_list[0]}_lambda", tup[0][2])
+        np.save(f"{save_path}/geo/geo_{idx}_node{nodes_list[1]}_lambda", tup[1][2])
+        np.save(f"{save_path}/geo/geo_{idx}_node{nodes_list[2]}_lambda", tup[2][2])
+    
+    print(f"Time elapsed: {time.time() - start}", flush=True)
+    print("done", flush=True)
+
+def init_comm_matrix(node_num, graph, load_path=None):
+    if load_path is None:
+        if graph == "exponential":
+            undir_graph = Exponential_graph(
+                node_num
+            ).undirected()  # generate the undirected graph
+        elif graph == "grid":
+            undir_graph = Grid_graph(int(math.sqrt(node_num))).undirected()
+        elif graph == "geometric":
+            undir_graph = Geometric_graph(
+                node_num
+            ).undirected(0.8)
+        elif graph == "fully_connected":
+            undir_graph = Fully_connected_graph(
+                node_num
+            ).undirected()
+
+        # communication_matrix = Weight_matrix(
+        #     undir_graph
+        # ).column_stochastic()  # generate the communication matrix
         communication_matrix = Weight_matrix(undir_graph).row_stochastic()
         communication_matrix = convert_to_doubly_stochastic(communication_matrix, int(1e4), 1e-7)
-        norms.append(spectral_norm(communication_matrix))
-
-    print(f"max norm = {max(norms)}")
-    print(f"min norm = {min(norms)}")
-    print(f"avg norm = {sum(norms)/len(norms)}")
-    print(f"{'-'*50}")
+    else:
+        communication_matrix = np.load(load_path)
+        print(f"loaded communication matrix from {load_path}")
+    
+    return communication_matrix
