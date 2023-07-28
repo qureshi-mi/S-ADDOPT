feat: fix stage lr & full batch gradient

LogisticRegression/Algos.py
@@ -33,6 +33,7 @@ def centralized_algo(
     C_lr,
     C_lr_dec,
     C_lr_list,
+    C_lr_dec_epochs,
     C_batch_size,
     CEPOCH_base,
     exp_name,
@@ -50,6 +51,7 @@ def centralized_algo(
     stop_at_convergence,
     algo,
     gap_type,
+    use_smoother,
 ):
     exp_save_path = f"{exp_log_path}/central_{algo}"
     initDir(exp_save_path)
@@ -71,7 +73,7 @@ def centralized_algo(
             params.append((CEPOCH_base[idx], bz, 0))
     else:
         for idx, bz in enumerate(C_batch_size):
-            for lr in C_lr:
+            for idx_lr, lr in enumerate(C_lr):
                 params.append((CEPOCH_base[idx], bz, lr))
 
     exp_names = []
@@ -100,7 +102,7 @@ def centralized_algo(
             )
 
         if algo == "SGD":
-            theta, theta_opt, F_opt, gradients = copt.SGD(
+            theta, theta_opt, F_opt = copt.SGD(
                 logis_model,
                 lr,
                 epoch,
@@ -114,9 +116,10 @@ def centralized_algo(
                 error_lr,
                 stop_at_converge=stop_at_convergence,
                 lr_list=C_lr_list,
+                lr_dec_epochs=C_lr_dec_epochs,
             )
         elif algo == "CRR":
-            theta, theta_opt, F_opt, gradients = copt.C_RR(
+            theta, theta_opt, F_opt = copt.C_RR(
                 logis_model,
                 lr,
                 epoch,
@@ -130,6 +133,7 @@ def centralized_algo(
                 error_lr,
                 stop_at_converge=stop_at_convergence,
                 lr_list=C_lr_list,
+                lr_dec_epochs=C_lr_dec_epochs,
             )
 
         np.save(
@@ -146,7 +150,7 @@ def centralized_algo(
         np.save(
             f"{exp_save_path}/{algo}_gap_epoch{epoch}_bz{bz}_lr{lr:.6f}_F.npy", res_F_F
         )
-        res_F_grad = error_lr.cost_gap_path(gradients, gap_type="grad")
+        res_F_grad = error_lr.cost_gap_path(theta, gap_type="grad")
         np.save(
             f"{exp_save_path}/{algo}_gap_epoch{epoch}_bz{bz}_lr{lr:.6f}_grad.npy",
             res_F_grad,
@@ -170,6 +174,7 @@ def centralized_algo(
         plot_every,
         mark_every,
         plot_first,
+        use_smoother,
     )
     plot_figure_path(
         exp_save_path,
@@ -183,6 +188,7 @@ def centralized_algo(
         plot_every,
         mark_every,
         plot_first,
+        use_smoother,
     )
     plot_figure_path(
         exp_save_path,
@@ -196,6 +202,7 @@ def centralized_algo(
         plot_every,
         mark_every,
         plot_first,
+        use_smoother,
     )
 
     exp_names = [f"central_{algo}/{name}" for name in exp_names]
@@ -208,6 +215,7 @@ def decentralized_algo(
     D_lr,
     D_lr_dec,
     D_lr_list,
+    D_lr_dec_epochs,
     D_batch_size,
     DEPOCH_base,
     communication_matrix,
@@ -229,6 +237,7 @@ def decentralized_algo(
     stop_at_convergence,
     algo,
     gap_type,
+    use_smoother,
 ):
     exp_save_path = f"{exp_log_path}/{algo}"
     initDir(exp_save_path)
@@ -282,7 +291,7 @@ def decentralized_algo(
             )
 
         if algo == "DSGD":
-            theta_D, gradients = dopt.D_SGD(
+            theta_D = dopt.D_SGD(
                 logis_model,
                 communication_matrix,
                 lr,
@@ -299,10 +308,11 @@ def decentralized_algo(
                 error_lr,
                 stop_at_converge=stop_at_convergence,
                 comm_type=comm_type,
-
+                lr_list=D_lr_list,
+                lr_dec_epochs=D_lr_dec_epochs,
             )
         elif algo == "DRR":
-            theta_D, gradients = dopt.D_RR(
+            theta_D = dopt.D_RR(
                 logis_model,
                 communication_matrix,
                 lr,
@@ -319,6 +329,8 @@ def decentralized_algo(
                 error_lr,
                 stop_at_converge=stop_at_convergence,
                 comm_type=comm_type,
+                lr_list=D_lr_list,
+                lr_dec_epochs=D_lr_dec_epochs,
             )
 
         res_F_D = error_lr.cost_gap_path(theta_D, gap_type="theta")
@@ -333,7 +345,7 @@ def decentralized_algo(
             f"{exp_save_path}/{algo}_gap_epoch{epoch}_bz{bz}_lr{lr:.6f}_ur{cr}_F.npy",
             res_F_D_F,
         )
-        res_F_D_grad = error_lr.cost_gap_path(gradients, gap_type="grad")
+        res_F_D_grad = error_lr.cost_gap_path(theta_D, gap_type="grad")
         np.save(
             f"{exp_save_path}/{algo}_gap_epoch{epoch}_bz{bz}_lr{lr:.6f}_ur{cr}_grad.npy",
             res_F_D_grad,
@@ -357,6 +369,7 @@ def decentralized_algo(
         plot_every,
         mark_every,
         plot_first,
+        use_smoother,
     )
     plot_figure_path(
         exp_save_path,
@@ -370,6 +383,7 @@ def decentralized_algo(
         plot_every,
         mark_every,
         plot_first,
+        use_smoother,
     )
     plot_figure_path(
         exp_save_path,
@@ -383,6 +397,7 @@ def decentralized_algo(
         plot_every,
         mark_every,
         plot_first,
+        use_smoother,
     )
 
     exp_names = [f"{algo}/{name}" for name in exp_names]

LogisticRegression/Optimizers/COPTIMIZER.py
@@ -44,7 +44,7 @@ def CGD(pr, learning_rate, K, theta_0):
     return theta, theta_opt, F_opt
 
 
-def SGD(pr, learning_rate, K, theta_0, batch_size, lr_dec, lr_staged, save_path, exp_name, save_every, error_lr_0, stop_at_converge=False, lr_list=None):
+def SGD(pr, learning_rate, K, theta_0, batch_size, lr_dec, lr_staged, save_path, exp_name, save_every, error_lr_0, stop_at_converge=False, lr_list=None, lr_dec_epochs=None):
     """
     Centralized mini-batch SGD Optimizer. This optimizer trains on all
     data globally in a batched manner.
@@ -69,7 +69,6 @@ def SGD(pr, learning_rate, K, theta_0, batch_size, lr_dec, lr_staged, save_path,
     """
     theta_copy = cp.deepcopy(theta_0)
     theta = [theta_copy]
-    gradients = []
     if lr_staged:
         lr_idx = 0
         learning_rate = lr_list[lr_idx]
@@ -85,14 +84,22 @@ def SGD(pr, learning_rate, K, theta_0, batch_size, lr_dec, lr_staged, save_path,
             assert lr_staged is False
             learning_rate = 1 / (50*k + 400)
         
-        if lr_staged and lr_idx < len(lr_list) - 1 and k - lr_change_round > 20:
-            assert lr_list is not None
-            assert lr_dec is False
-            if model_converged(pr, theta):
-                lr_idx += 1
+        if lr_dec_epochs is None:
+            if lr_staged and lr_idx < len(lr_list) - 1 and k - lr_change_round > 20:
+                assert lr_list is not None
+                assert lr_dec is False
+                if model_converged(pr, theta):
+                    lr_idx += 1
+                    learning_rate = lr_list[lr_idx]
+                    lr_change_round = k
+                    print(f"Learning Rate Decreased to {learning_rate} at {k} round")
+        else:
+            if k in lr_dec_epochs:
+                assert lr_list is not None
+                assert lr_dec is False
+                lr_idx = lr_dec_epochs.index(k) + 1
                 learning_rate = lr_list[lr_idx]
-                lr_change_round = k
-                print(f"Learning Rate Decreased to {learning_rate} at {k} round")
+                print(f"Learning Rate Decreased to {learning_rate} at {k} round - {lr_idx}th decrease")
 
         temp = theta[-1]
         # gradient updates happening in one local training round
@@ -106,7 +113,6 @@ def SGD(pr, learning_rate, K, theta_0, batch_size, lr_dec, lr_staged, save_path,
             temp = temp - learning_rate * grad
 
         theta.append(temp)
-        gradients.append(grad)
 
         ut.monitor("SGD", k, K, track_time)
         if (k + 1) % save_every == 0:
@@ -130,10 +136,10 @@ def SGD(pr, learning_rate, K, theta_0, batch_size, lr_dec, lr_staged, save_path,
     print(f"Time Span: {time.time() - start}")
     theta_opt = theta[-1]
     F_opt = pr.F_val(theta[-1])
-    return theta, theta_opt, F_opt, gradients
+    return theta, theta_opt, F_opt
 
 
-def C_RR(pr, learning_rate, K, theta_0, batch_size, lr_dec, lr_staged, save_path, exp_name, save_every, error_lr_0, stop_at_converge=False, lr_list=None):
+def C_RR(pr, learning_rate, K, theta_0, batch_size, lr_dec, lr_staged, save_path, exp_name, save_every, error_lr_0, stop_at_converge=False, lr_list=None, lr_dec_epochs=None):
     """
     Centralized Random Reshuflling Optimizer.
 
@@ -151,7 +157,6 @@ def C_RR(pr, learning_rate, K, theta_0, batch_size, lr_dec, lr_staged, save_path
     """
     theta_copy = cp.deepcopy(theta_0)
     theta = [theta_copy]
-    gradients = []
     if lr_staged:
         lr_idx = 0
         learning_rate = lr_list[lr_idx]
@@ -164,14 +169,22 @@ def C_RR(pr, learning_rate, K, theta_0, batch_size, lr_dec, lr_staged, save_path
             assert lr_staged is False
             learning_rate = 1 / (50*k + 400)
 
-        if lr_staged and lr_idx < len(lr_list) - 1 and k - lr_change_round > 20:
-            assert lr_list is not None
-            assert lr_dec is False
-            if model_converged(pr, theta):
-                lr_idx += 1
+        if lr_dec_epochs is None:
+            if lr_staged and lr_idx < len(lr_list) - 1 and k - lr_change_round > 20:
+                assert lr_list is not None
+                assert lr_dec is False
+                if model_converged(pr, theta):
+                    lr_idx += 1
+                    learning_rate = lr_list[lr_idx]
+                    lr_change_round = k
+                    print(f"Learning Rate Decreased to {learning_rate} at {k} round")
+        else:
+            if k in lr_dec_epochs:
+                assert lr_list is not None
+                assert lr_dec is False
+                lr_idx = lr_dec_epochs.index(k) + 1
                 learning_rate = lr_list[lr_idx]
-                lr_change_round = k
-                print(f"Learning Rate Decreased to {learning_rate} at {k} round")
+                print(f"Learning Rate Decreased to {learning_rate} at {k} round - {lr_idx}th decrease")
             
         cnt = 0
         permutation = np.random.permutation(pr.N)
@@ -186,7 +199,6 @@ def C_RR(pr, learning_rate, K, theta_0, batch_size, lr_dec, lr_staged, save_path
             cnt = cnt + batch_size
 
         theta.append(temp)
-        gradients.append(grad)
 
         ut.monitor("C_RR", k, K, track_time)
         if (k + 1) % save_every == 0:
@@ -210,7 +222,7 @@ def C_RR(pr, learning_rate, K, theta_0, batch_size, lr_dec, lr_staged, save_path
     print(f"Time Span: {time.time() - start}")
     theta_opt = theta[-1]
     F_opt = pr.F_val(theta[-1])
-    return theta, theta_opt, F_opt, gradients
+    return theta, theta_opt, F_opt
 
 
 ## Centralized gradient descent with momentum

LogisticRegression/Problems/log_reg_cifar.py
@@ -13,11 +13,26 @@ import cifar10
 
 # Labels of different classes to select from:
 # airplane = 0, automobile = 1, bird = 2, cat = 3, deer = 4, dog = 5, frog = 6, horse = 7, ship = 8, truck = 9;
+cifar10_classes = {
+    0: "airplane",
+    1: "automobile",
+    2: "bird",
+    3: "cat",
+    4: "deer",
+    5: "dog",
+    6: "frog",
+    7: "horse",
+    8: "ship",
+    9: "truck",
+}
 
 class LR_L4( object ):
     def __init__(self, n_agent, class1 = 0, class2 = 1, balanced = True, limited_labels = False, nonconvex = False ):
         self.class1 = class1
         self.class2 = class2
+        print( 'class1: ', cifar10_classes[class1] )
+        print( 'class2: ', cifar10_classes[class2] )
+
         self.limited_labels = limited_labels
         self.n = n_agent 
         self.balanced = balanced
@@ -33,8 +48,8 @@ class LR_L4( object ):
         self.b = int(self.N/self.n)           ## average local samples
 
         self.nonconvex = nonconvex
-        if self.nonconvex:
-            self.reg = 0.2
+        self.reg = 0.2
+        print( 'reg', self.reg )
 
     def load_data(self):
 #         (trainX, trainY), (testX, testY) = cifar10.load_data()         # use this if you want have keras installed
@@ -121,6 +136,19 @@ class LR_L4( object ):
                 f_val += np.sum(temp2[i])/self.data_distr[i]
             reg_val = (self.reg/2) * (LA.norm(theta) ** 2) 
             return f_val/self.n + reg_val
+
+    def F_grad(self, theta):          ##  gradient of the objective function at theta
+        if self.balanced == True:
+            temp1 = np.exp( np.matmul(self.X_train,theta) * (-self.Y_train)  )
+            temp2 = ( temp1/(temp1+1) ) * (-self.Y_train)
+            grad = self.X_train * temp2[:,np.newaxis]
+            if self.nonconvex:
+                denominator = np.power(theta,2)
+                denominator = np.power(denominator + 1, 2)
+                grad_reg = 2*theta / denominator
+            else:
+                grad_reg = 2*theta
+            return np.sum(grad, axis = 0)/self.N + self.reg/2 * grad_reg
             
         
     def localgrad(self, theta, idx, j = None, permute = None, permute_flag = False ):  ## idx is the node index, j is local sample index

LogisticRegression/analysis.py
@@ -38,10 +38,14 @@ class error:
         return self.pr.F_val(theta) - self.F_opt
 
     def grad_gap_path(self, iterates):
-        iterates = np.array( iterates )
-        if iterates.ndim == 2:  # if iterates is a 2D array, reshape it to 3D. simulate a network with only one node
-            iterates = iterates[:,np.newaxis,:]
-        norms = np.apply_along_axis( LA.norm, 2, iterates )**2
+        grads = []
+        for k in range(len(iterates)):
+            grads.append( self.pr.F_grad(iterates[k]) )
+        grads = np.array(grads)
+
+        if grads.ndim == 2:  # if grads is a 2D array, reshape it to 3D. simulate a network with only one node
+            grads = grads[:,np.newaxis,:]
+        norms = np.apply_along_axis( LA.norm, 2, grads )**2
         return np.sum( norms, axis = 1 ) / norms.shape[1]
     
     def cost_gap_path(self, iterates, gap_type = "F"):